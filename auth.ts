b49041a105e7914a92d8565a5b833f09b90d2e68
Burak Bilgili <burak.bilgili@icloud.com>
2025-01-03 13:19:22 +0300

    setup next auth

---
+++ auth.ts
@@ -0,0 +1,69 @@
+import NextAuth from 'next-auth';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import CredentialProvider from 'next-auth/providers/credentials';
+
+import prisma from './database';
+import { compare } from './lib/encrypt';
+
+import type { NextAuthConfig } from 'next-auth';
+
+export const config: NextAuthConfig = {
+  pages: {
+    signIn: '/sign-in',
+    error: '/sign-in',
+  },
+
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60,
+  },
+
+  adapter: PrismaAdapter(prisma),
+
+  providers: [
+    CredentialProvider({
+      credentials: {
+        email: { type: 'email' },
+        password: { type: 'password' },
+      },
+
+      async authorize(credentials) {
+        if (credentials === null) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email as string },
+        });
+
+        if (user && user.password) {
+          const isMatch = await compare(
+            credentials.password as string,
+            user.password
+          );
+
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+
+        return null;
+      },
+    }),
+  ],
+
+  callbacks: {
+    async session({ session, user, trigger, token }) {
+      session.user.id = token.sub as string;
+
+      if (trigger === 'update') session.user.name = user.name;
+
+      return session;
+    },
+  },
+};
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config);
