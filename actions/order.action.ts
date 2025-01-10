5e662fd3f343df6193ca0001ce2391a18395cd56
Burak Bilgili <burak.bilgili@icloud.com>
2025-01-10 14:13:27 +0300

    get summary action

--- actions/order.action.ts
+++ actions/order.action.ts
@@ -2,6 +2,8 @@
 
 import { revalidatePath } from 'next/cache';
 
+import { Prisma } from '@prisma/client';
+
 import prisma from '@/database';
 
 import { auth } from '@/auth';
@@ -270,4 +272,38 @@ const updateOrderAsPaid = async ({ id, paymentResult }: UpdateAsPaid) => {
       },
     });
   });
+};
+
+export const getSummary = async () => {
+  const usersCount = await prisma.user.count();
+  const ordersCount = await prisma.order.count();
+  const productsCount = await prisma.product.count();
+
+  const totalSales = await prisma.order.aggregate({
+    _sum: { totalPrice: true },
+  });
+
+  const salesRaw = await prisma.$queryRaw<
+    Array<{ month: string; totalSales: Prisma.Decimal }>
+  >`SELECT to_char("createdAt", 'MM/YY') as "month", sum("totalPrice") as "totalSales" FROM "Order" GROUP BY to_char("createdAt", 'MM/YY')`;
+
+  const sales = salesRaw.map((entry) => ({
+    month: entry.month,
+    totalSales: Number(entry.totalSales),
+  }));
+
+  const latestSales = await prisma.order.findMany({
+    orderBy: { createdAt: 'desc' },
+    include: { user: { select: { name: true } } },
+    take: 6,
+  });
+
+  return {
+    usersCount,
+    ordersCount,
+    productsCount,
+    totalSales,
+    sales,
+    latestSales,
+  };
 };
